d596f304e4b9cedbae7a682699b012af508e4ce6
daniel croucher <Shozuki>
2019-10-09 21:41:35 +0100

    Early Pagination Feature Implementation

--- src/app/shows/show-list/show-list.component.ts
+++ src/app/shows/show-list/show-list.component.ts
@@ -10,8 +10,11 @@ export class ShowListComponent implements OnInit {
 
   pageTitle: string = 'Tv Shows';
 
-  shows:Array<any> = [];
-  genres:Array<any> = [];
+  shows: Array<any> = [];
+  genres: Array<any> = [];
+
+  pageNumber: number = 1;
+  pages: Array<number>;
 
   constructor(
     private tmdb: TmdbApiService
@@ -28,6 +31,7 @@ export class ShowListComponent implements OnInit {
 					this.genres = data.genres;
 					this.genres.forEach(genre => genreLookup.set(genre.id, genre));
 					this.shows = res.results;
+					this.pages = this.paginate(this.pageNumber, res.total_results, 5);
 					this.shows.forEach(show => {
 						show.genre_names = show.genre_ids
 							.filter(id => genreLookup.has(id))
@@ -36,7 +40,38 @@ export class ShowListComponent implements OnInit {
 						});
 				});
 		});
+  }
 
+  changePage(page) {
+	  console.log(page);
+  }
+
+  // Calculate pages from current page, total cut off and return an array of page numbers
+  paginate(currPage, totalPages, cutOff) {
+	let start, end;
+	const lower = Math.floor(cutOff / 2);
+	const upper = Math.ceil(cutOff / 2);
+	const pages = [];
+
+	if (totalPages < cutOff) {
+		start = 1;
+		end = totalPages;
+	} else if (currPage >= 1 && currPage <= upper) {
+		start = 1;
+		end = cutOff;
+	}  else if ((currPage + lower) >= totalPages) {
+		start = totalPages - cutOff;
+		end = totalPages;
+	} else {
+		start = (currPage - upper);
+		end = (currPage + lower);
+	}
+
+	for (let i = start; i <= end; i++) {
+		pages.push(i);
+	}
+
+	return pages;
   }
 
 }
