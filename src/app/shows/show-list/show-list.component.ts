d3e5ab40b918cbd1cdd7913620679d15055bc0f8
daniel croucher <Shozuki>
2019-10-12 20:56:31 +0100

    Refactored Show List Component

--- src/app/shows/show-list/show-list.component.ts
+++ src/app/shows/show-list/show-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TmdbApiService } from '../../core/services/tmdb-api.service';
+import { mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-show-list',
@@ -14,40 +15,53 @@ export class ShowListComponent implements OnInit {
   genres: Array<any> = [];
 
   currentPage: number = 1;
+  totalPages: number = 1;
   pages: Array<number>;
 
   constructor(
     private tmdb: TmdbApiService
   ) {}
 
-  ngOnInit() {
-
-    this.tmdb.getTopRatedShows()
-    	.subscribe((res:any) => {
-			this.tmdb.getTvGenres()
-				.subscribe((data:any) => {
-					// Genre Map lookup table
-					const genreLookup = new Map();
-					this.genres = data.genres;
-					this.genres.forEach(genre => genreLookup.set(genre.id, genre));
-					this.shows = res.results;
-					this.pages = this.paginate(this.currentPage, res.total_pages, 5);
-					this.shows.forEach(show => {
-						show.genre_names = show.genre_ids
-							.filter(id => genreLookup.has(id))
-							.map(id => genreLookup.get(id).name)
-							.join(', ');
-						});
-				});
-		});
+  public ngOnInit() {
+		this.getShows(1);
   }
 
-  changePage(page) {
-	  console.log(page);
+
+  /**
+   * Get show results by page number
+   * @param {} page
+   */
+  public getShows(page) {
+	this.tmdb.getTopRatedShows(page)
+		.pipe(
+			mergeMap(shows => {
+				this.shows = shows.results;
+				this.totalPages = shows.total_pages;
+				this.pages = this.paginate(page, shows.total_pages, 5);
+				return this.tmdb.getTvGenres();
+			})
+		).subscribe(gens => {
+			// Genre Map lookup table
+			const genreLookup = new Map();
+			this.genres = gens.genres;
+			this.genres.forEach(genre => genreLookup.set(genre.id, genre));
+			this.currentPage = page;
+			this.shows.forEach(show => {
+				show.genre_names = show.genre_ids
+					.filter(id => genreLookup.has(id))
+					.map(id => genreLookup.get(id).name)
+					.join(', ');
+			});
+		});		
   }
 
-  // Calculate pages from current page, total cut off and return an array of page numbers
-  paginate(currPage, totalPages, range) {
+  /**
+   * Calculate pages from current page, total and range, then return an array of page numbers
+   * @param  {} currPage
+   * @param  {} totalPages
+   * @param  {} range
+   */
+  public paginate(currPage, totalPages, range) {
 	let start, end;
 	const lower = Math.floor(range / 2);
 	const upper = Math.ceil(range / 2);
