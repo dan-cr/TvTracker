71c5402d0cc7aeed96663d2e8ccc67bf4157c51c
dan-cr <Shozuki>
2020-02-09 21:58:10 +0000

    Add extra optional properties to Show interface

--- src/app/core/models/show.ts
+++ src/app/core/models/show.ts
@@ -7,12 +7,16 @@ export interface Show {
     genre_names?: string;
     id: number;
     name: string;
+    next_episode_to_air?: {air_date: string}
+    number_of_seasons?: any;
     origin_country: Array<string>;
     original_language: string;
     original_name: string;
     overview:string;
     popularity: number;
     poster_path: string;
+    status?: any;
+    seasons?: any;
     vote_average: number;
     vote_count: number;
 }
